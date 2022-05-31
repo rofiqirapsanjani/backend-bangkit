09f735fd12032b2aa7e0757dbb751080ea90d68d
rofiqirapsanjani <achmad.rofiqi@ui.ac.id>
2022-05-31 22:03:39 +0700

    fitur 1

---
+++ env.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://bangkit-backend:Bangkit@cluster0.9rwlcvf.mongodb.net/Bangkit?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("thriftee");
+    const ratings = database.collection("users");
+    const cursor = ratings.find();
+    await cursor.forEach(doc => console.dir(doc));
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
--- routes/auth.js
+++ routes/auth.js
@@ -1,44 +1,70 @@
-const router = require ("express").Router();
-const User = require("..//models/User");
-const CryptoJS = require("crypto-js")
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
-//register
-router.post("/register", async (req,res)=>{
-    const newUser = new User({
-        username : req.body.username,
-        email : req.body.email,
-
-        password : CryptoJS.AES.encrypt(req.body.password, "BANGKIT-PASS").toString(),
-
-    });
-    try{
+//REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+  try {
     const savedUser = await newUser.save();
-    res.status(200).json(savedUser);
-    }catch(err){
-        res.status(500).json(err);
+    console.log(savedUser)
+    res.status(201).json(savedUser);
+  } catch (err) {
+    console.log('failed to register')
+    // res.status(500).json(err);
+  }
+});
 
+//LOGIN
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne(
+            {
+                username: req.body.username
+            }
+        );
+        if(!user){ 
+            return res.status(401).json("Wrong User Name");
+        }
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        const inputPassword = req.body.password;
+                
+        if(originalPassword != inputPassword){ 
+            return res.status(401).json("Wrong Password");
+        }
+
+        const accessToken = jwt.sign(
+        {
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+        );
+            {expiresIn:"3d"}
+  
+        const { password, ...others } = user._doc;  
+        res.status(200).json({...others, accessToken});
+        // res.status(200).json(others);
+
+    }catch(err){
+        console.log('failed to login')
+        return res.status(500).json(err);
     }
 
 });
-
-//login
-router.post("/login", async(req,res)=>{
-    try{
-        const user = await User.findOne({username : req.body.username});
-        if(!user){
-            res.status(400).json({
-                message: "user not found"
-            });
-        }
-        const hashedPassword = CryptoJS.AES.decrypt(user.password, "BANGKIT-PASS");
-        const password = hashedPassword.toString(CryptoJS.enc.Utf8);
-
-    
-    }catch(err){
-        res.status(500).json(err);
-
-    }
-
-})
 
 module.exports = router;
